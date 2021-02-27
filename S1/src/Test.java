eaf17bf8d53a7593c9b6d75db1a474ffab64e71f
Mariana Serban <marianaserban734@gmail.com>
2021-02-27 12:18:06 +0200

    alt commit de test

--- S1/src/Test.java
+++ S1/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hei din nou");
 		System.out.println("hei");
+		System.out.print("scriu pt alt commit");
 		//ctrl +  spatiu
 	}
 
