aef9917b92820d293c7b5ae372736dc8de886cfb
Mariana Serban <marianaserban734@gmail.com>
2021-03-18 15:35:02 +0200

    notification service

--- Seminar4/src/ro/ase/csie/cts/seminar4/banking/Person.java
+++ Seminar4/src/ro/ase/csie/cts/seminar4/banking/Person.java
@@ -3,13 +3,85 @@ package ro.ase.csie.cts.seminar4.banking;
 import java.util.Date;
 
 public class Person {
+
 	private String name;
-	private String adress;
+
+	private String address;
+
 	private Date birthDate;
+
+	private Integer age;
+
 	private long salary;
+
 	private String email;
+
 	private String mobile;
-	
+
+	private NotificationType notificationType;
+
+	public static enum NotificationType {
+		EMAIL {
+			@Override
+			public NotificationService getNotificationService() {
+				return new EmailNotificationService();
+			}
+		},
+		SMS
+		{
+			@Override
+			public NotificationService getNotificationService() {
+				return new SMSNotificationService();
+			}
+		};
+
+		public abstract NotificationService getNotificationService();
+	}
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getMobile() {
 		return mobile;
 	}
@@ -17,47 +89,21 @@ public class Person {
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
-	private NotificationType notificationType;
-	
-	public static enum NotificationType{
-		EMAIL,
-		SMS
+
+	public NotificationType getNotificationType() {
+		return notificationType;
 	}
-	
-	public Person(String name) {
-		this.name=name;
+
+	public void setNotificationType(NotificationType notificationType) {
+		this.notificationType = notificationType;
 	}
-	
-	public String getName() {
-		return name;
+
+	public Integer getAge() {
+		return age;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setAge(Integer age) {
+		this.age = age;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAdress() {
-		return adress;
-	}
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
-	public Date getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-	
-	
+
 }
