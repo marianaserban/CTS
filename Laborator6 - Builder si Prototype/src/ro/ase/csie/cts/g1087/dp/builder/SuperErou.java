cac15889f1948111caaf1c8dee08981cec4ad579
Mariana Serban <marianaserban734@gmail.com>
2021-03-31 15:30:11 +0300

    Sfarsit seminar

--- Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/SuperErou.java
+++ Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/SuperErou.java
@@ -2,22 +2,22 @@ package ro.ase.csie.cts.g1087.dp.builder;
 
 public class SuperErou {
 	
-	String nume;
-	int puncteViata;
+	private String nume;
+	private int puncteViata;
 	
-	boolean esteNegativ;
-	boolean esteRanitGrav;
+	private boolean esteNegativ;
+	private boolean esteRanitGrav;
 	
-	InterfataArma armaStanga;
-	InterfataArma armaDreapa;
-	InterfataSuperPutere superPutere;
-	InterfataSuperPutere superSuperPutere;
+	private InterfataArma armaStanga;
+	private InterfataArma armaDreapa;
+	private InterfataSuperPutere superPutere;
+	private InterfataSuperPutere superSuperPutere;
 	
-	public SuperErou() {
+	private SuperErou() {
 		
 	}
 
-	public SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
+	private SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
 			InterfataArma armaDreapa, InterfataSuperPutere superPutere, InterfataSuperPutere superSuperPutere) {
 		super();
 		this.nume = nume;
@@ -31,5 +31,55 @@ public class SuperErou {
 	}
 	
 	
+	
+	//daca n ar fi statica ar fi ca o metoda membra
+	//ca sa o accesez am nevoie de un obiect
+	//daca n ar fi statica n as avea cu sa o apelez
+	public static class SuperErouBuilder{
+		
+		SuperErou superErou;
+		
+		public SuperErouBuilder(String nume, int puncteViata) {
+			this.superErou=new SuperErou();
+			this.superErou.nume=nume;
+			this.superErou.puncteViata=puncteViata;
+		}
+		
+		//intoarce referinta catre bulder
+		public SuperErouBuilder esteNegativ() {
+			this.superErou.esteNegativ=true;
+			return this;
+		}
+		
+		public SuperErouBuilder esteRanit() {
+			this.superErou.esteRanitGrav=true;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaStanga(InterfataArma arma) {
+			this.superErou.armaStanga=arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaDreapta(InterfataArma arma) {
+			this.superErou.armaDreapa=arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperPutere(InterfataSuperPutere superPutere) {
+			this.superErou.superPutere=superPutere;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperSuperPutere(InterfataSuperPutere superSuperPutere) {
+			this.superErou.superSuperPutere=superSuperPutere;
+			return this;
+		}
+		
+		public SuperErou buid() {
+			return this.superErou;
+		}
+		
+	}
 	
 }
--- Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/TestBuilder.java
+++ Laborator6 - Builder si Prototype/src/ro/ase/csie/cts/g1087/dp/builder/TestBuilder.java
@@ -6,14 +6,37 @@ public class TestBuilder {
 	public static void main(String[] args) {
 		
 		//1 construiesti obiectul
-		SuperErou superErou=new SuperErou();
+		//SuperErou superErou=new SuperErou();
 		//2. initializarea atributelor
-		superErou.nume="Superman";
+		//superErou.nume="Superman";
 		
-		SuperErou superErou2=
-				new SuperErou("Batman", 100, false, false, 
-						new Arma(), null, null, null);
+//		SuperErou superErou2=
+//				new SuperErou("Batman", 100, false, false, 
+//						new Arma(), null, null, null);
 		
+		SuperErou superman=
+				new SuperErou.SuperErouBuilder("Superman", 100)
+				.setArmaDreapta(new Arma())
+				.setArmaStanga(new Arma())
+				.buid();
+		//avem referinta la builder, pot apela metode din ele
+		//am intors referinta pot apela altele
+		
+		SuperErou joker=
+				new SuperErou.SuperErouBuilder("Joker", 200)
+				.esteNegativ()
+				.esteRanit()
+				.setArmaDreapta(new Arma())
+				.buid();
+		
+		//facem directorul in situatia asta
+		//definim o metoda construiesteJoker() ca sa nu mai copiem cod
+		SuperErou joker2=
+				new SuperErou.SuperErouBuilder("Joker", 200)
+				.esteNegativ()
+				.esteRanit()
+				.setArmaDreapta(new Arma())
+				.buid();
 		
 		
 	}
