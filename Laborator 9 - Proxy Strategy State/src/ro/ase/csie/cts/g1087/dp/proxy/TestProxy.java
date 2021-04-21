dcd2bf44e49d429861385e792be4bdc1696ecaf1
Mariana Serban <marianaserban734@gmail.com>
2021-04-21 14:42:05 +0300

    Implementare Proxy

---
+++ Laborator 9 - Proxy Strategy State/src/ro/ase/csie/cts/g1087/dp/proxy/TestProxy.java
@@ -0,0 +1,28 @@
+package ro.ase.csie.cts.g1087.dp.proxy;
+
+public class TestProxy {
+
+	public static void main(String[] args) {
+		
+		InterfataModulLogin login=new ModulLogin("10.0.0.1", 127);
+		
+		login=new ProxyLogin(login);
+		
+		if(login.login("admin", "admin12")) {
+			System.out.println("hello admin");
+		}
+		
+		String[]paroleStrings=new String[] {"1234",
+			"123456", "admin", "root", "admin12"};
+		
+		for(String parola:paroleStrings) {
+			if(login.login("admin", parola)) {
+				System.out.println("Parola gasita: "+parola);
+			}
+		}
+		
+		System.out.println("Log out");
+
+	}
+
+}
